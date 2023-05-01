3b74e6b000ce42391495abeeba89688dc8550347
Robert Findlay <103613414@student.swin.edu.au>
2023-05-01 23:18:18 +1000

    Write TSK layer tools that convert runner 'matrix' to meaningful data

---
+++ src/main/sleuthkit/file-name-tools.ts
@@ -0,0 +1,9 @@
+import { ipcMain } from 'electron';
+import { runTool } from './runner'
+
+ipcMain.on('file-name:listFiles', async(event, arg) => {
+  runTool(`fls ${arg[0]} -o ${arg[1]}`, async (matrix) => {
+    console.log(matrix);
+    event.reply('file-name:listFiles', matrix);
+  })  
+});
