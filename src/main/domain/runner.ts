de4ef77f0b6d7d714889303638a8db5c7a49137b
Mafaz Abrar Jan Chowdhury <mafazabrar@gmail.com>
2023-08-06 21:52:45 +1000

    Fixed runner

--- src/main/domain/runner.ts
+++ src/main/domain/runner.ts
@@ -1,34 +1,42 @@
 import { exec } from 'node:child_process';
+import util from 'node:util';
 
-async function runVolumeSystemTool(bin: string, callback: (matrix: string[][]) => void) {
+const promisifiedExec = util.promisify(exec);
+
+async function runVolumeSystemTool(
+  bin: string,
+  callback: (matrix: string[][]) => void
+) {
   exec(bin, (error, stdout, stderr) => {
-    if(error) {
+    if (error) {
       console.log(error);
       return;
     }
     let lines = stdout.split('\n');
-    let matrix = lines.map(line => line.split(/\s+/));
+    let matrix = lines.map((line) => line.split(/\s+/));
     callback(matrix);
   });
 }
 
-async function runFileSystemTool(bin: string, callback: (matrix: string[][]) => void) {
+async function runFileSystemTool(
+  bin: string,
+  callback: (matrix: string[][]) => void
+) {
   exec(bin, (error, stdout, stderr) => {
-    if(error) {
+    if (error) {
       console.log(error);
       return;
     }
     let lines = stdout.split('\n');
-    let matrix = lines.map(line => line.split(/\s+/));
+    let matrix = lines.map((line) => line.split(/\s+/));
     callback(matrix);
   });
 }
 
-async function runOtherCliTool(bin: string) {
+async function runCliTool(cmdString: string) {
+  const { stdout, stderr } = await promisifiedExec(cmdString);
 
+  return { stdout, stderr };
 }
 
-export { 
-  runVolumeSystemTool,
-  runFileSystemTool 
-};
+export { runVolumeSystemTool, runFileSystemTool };
