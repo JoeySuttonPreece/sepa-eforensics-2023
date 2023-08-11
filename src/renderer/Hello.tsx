c7b817e5d48ae475c3a67b55844ba83f1d25c99b
ActuallyAdequate <actuallyadequate@gmail.com>
2023-08-11 15:34:44 +1000

    feat-partial: T005 frontend Renamed Files Table Added React hooks not done due to prequisit orchestrator (can't get required params)

--- src/renderer/Hello.tsx
+++ src/renderer/Hello.tsx
@@ -1,11 +1,14 @@
 import { Partition, PartitionTable } from '../main/domain/volume-system-tools'
 import { useState } from 'react';
 import PartitionTableComponent from './PartitionTable';
+import RenamedFilesComponent from './RenamedFiles';
+import { RenamedFile } from 'main/domain/file-system-tools';
 
 export default function Hello() {
 
   const [MD5String, setMD5String] = useState('');
   const [PartitionTableData, setPartitionTableData] = useState<{header: string, partitionTable: PartitionTable | undefined}>({header: "", partitionTable: undefined });
+  const [RenamedFilesData, setRenamedFilesData] = useState<{header: string, renamedFiles: RenamedFile[] | undefined}>({header: "", renamedFiles: undefined });
 
   function getPartitions() {
     let imagePathInput: HTMLInputElement | null = document.getElementById('imagePath') as HTMLInputElement;
@@ -17,6 +20,10 @@ export default function Hello() {
     }
 
     window.electron.ipcRenderer.sendMessage('volume-system:getPartitions', [fileName]);
+  }
+
+  function getRenamed() {
+    //Need to wait for restructure as can't get parameters required from here.
   }
   
   function getMD5Hash() {
@@ -81,6 +88,7 @@ export default function Hello() {
         {MD5String}
       </p>
       <PartitionTableComponent header={PartitionTableData.header} partitionTable={PartitionTableData.partitionTable}/>
+      <RenamedFilesComponent header={RenamedFilesData.header} renamedFiles={RenamedFilesData.renamedFiles}/>
     </div>
   );
 } 
---
+++ src/renderer/RenamedFiles.tsx
@@ -0,0 +1,31 @@
+import { RenamedFile } from "main/domain/file-system-tools";
+
+const RenamedFilesComponent =  ({header, renamedFiles} : {header: string, renamedFiles: RenamedFile[]| undefined}) => {
+    return (
+        <table>
+            <caption>{header}</caption>
+            <tr>
+                <th>Inode</th>
+                <th>FilePath</th>
+                <th>True Ext.</th>
+                <th>Size</th>
+                <th>MAC Date</th>
+            </tr>
+            {
+                renamedFiles?.map((renamed) => {
+                    return (
+                        <tr>
+                            <td>{renamed.file.inode}</td>
+                            <td>{renamed.file.filepath}</td>
+                            <td>{renamed.true_ext}</td>
+                            <td>{renamed.file.modified.getUTCDate()}<br/>{renamed.file.access.getUTCDate()}<br/>{renamed.file.created.getUTCDate()}</td>
+                        </tr>
+                    );
+                })
+            }
+           
+        </table>
+    )
+}
+
+export default RenamedFilesComponent;
