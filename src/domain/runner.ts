9c89436036fe6b2ffe8a6675f2bd41ceafb5f1bc
Rob Findlay <103613414@student.swin.edu.au>
2023-08-17 12:04:32 +1000

    Create function stub for runBufferedCliTool

--- src/domain/runner.ts
+++ src/domain/runner.ts
@@ -13,3 +13,14 @@ export const runCliTool = async (bin: string): Promise<string> => {
     });
   });
 };
+
+// This function takes in a string `bin`, which is the command to be run
+// It also takes in a lineProcessor function. This function will be run for every line in the output of the command that is run
+// NOTE: The return value for lineProcessor, and this function as a whole are both strings, this is purely for early development / demonstration / testing. This will be updated to a different type down the line
+
+// This function will return the an array of all the processed data, as computed by each lineProcessor function
+export const runBufferedCliTool = async (bin: string, lineProcessor: (line: string) => string): Promise<string[]> {
+  return new Promise<string[]>((resolve, reject) => {
+    
+  }); 
+}
