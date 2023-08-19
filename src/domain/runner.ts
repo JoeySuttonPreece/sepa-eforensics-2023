96df3c809c7b3557dae8195b0bbe2ed3ffb723ac
JoeySuttonPreece <102568393@student.swin.edu.au>
2023-08-19 12:11:19 +1000

    feat: generic type for runBufferedCliTool and formatting

--- src/domain/runner.ts
+++ src/domain/runner.ts
@@ -15,42 +15,47 @@ export const runCliTool = async (bin: string): Promise<string> => {
   });
 };
 
-// This function takes in a string `bin`, which is the command to be run
-// It also takes in a lineProcessor function. This function will be run for every line in the output of the command that is run
-
-// This function will return the an array of all the processed data, as computed by each lineProcessor function
-export const runBufferedCliTool = async (bin: string, lineProcessor: (line: string) => any): Promise<any[]> => {
-  return new Promise<string[]>((resolve, reject) => {
+/**
+ * This function takes in a string `bin`, which is the command to be run.
+ * It also takes in a lineProcessor function. This function will be run for every line in the output of the command that is run.
+ *
+ * This function will return the an array of all the processed data, as computed by each lineProcessor function.
+ */
+export const runBufferedCliTool = async <Type>(
+  bin: string,
+  lineProcessor: (line: string) => Type
+): Promise<Type[]> => {
+  return new Promise<Type[]>((resolve, reject) => {
     // Spawn expects a command, and then an array of arguments - this just allows us to run commands the same way as runCliTool/exec
-    let command = bin.split(' ');
-    let process = spawn(command[0], command.slice(1));
+    const command = bin.split(' ');
+    const process = spawn(command[0], command.slice(1));
 
-    let outputArray = [];
+    const outputArray: Type[] = [];
 
-    // Create a readline interface based on the stdout stream produced by the spawned process - this is how we get output line by line    
-    let lineReader = createInterface({
+    // Create a readline interface based on the stdout stream produced by the spawned process - this is how we get output line by line
+    const lineReader = createInterface({
       input: process.stdout,
-      terminal: false
+      terminal: false,
     });
 
     // For every line, run the callback function and append its return value to the final outputArray
     lineReader.on('line', (line: string) => {
-      let processedLine = lineProcessor(line);
+      const processedLine = lineProcessor(line);
       outputArray.push(processedLine);
     });
 
     // Once the process is finished executing, we can 'return' the output array
     // One thing that I have no verified, is if the lineProcessor performs a long-running task, what actually happens here - theoretically this should wait for it to finish
     process.on('exit', (code: number) => {
-      if(code === 0) {
+      if (code === 0) {
         resolve(outputArray);
       } else {
-        reject(new Error(`Child process exited with code ${code}`))
+        reject(new Error(`Child process exited with code ${code}`));
       }
     });
 
     process.on('error', (err: Error) => {
       reject(err);
     });
-  }); 
-}
+  });
+};
