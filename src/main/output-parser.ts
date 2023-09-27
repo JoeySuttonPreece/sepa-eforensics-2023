443cbe2ad2a531a37aad2929a79c3aa86e47aeaf
ActuallyAdequate <actuallyadequate@gmail.com>
2023-09-27 14:23:25 +1000

    feat: T032 - printing pdf

--- src/main/output-parser.ts
+++ src/main/output-parser.ts
@@ -4,54 +4,264 @@ import { RenamedFile, File, KeywordFile } from 'domain/file-system-tools';
 import { ReportDetails } from 'domain/orchestrator';
 import { PartitionTable } from 'domain/volume-system-tools';
 import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
-type Writer = (data: string, section: string) => void;
+type Writer = (data: string | Buffer, section: string) => void;
 
-export const PrintPDF = (arrayBuffer: ArrayBuffer, destination: string) => {
-  let filename = path.join(destination, `aeas-report.pdf`);
-  fs.writeFileSync(filename, Buffer.from(arrayBuffer));
-};
-
-// format can be stdout, csv, json
-export const PrintText = (
+// format can be stdout, csv, json, pdf
+export const Print = (
   output: ReportDetails,
   format: string,
   destination: string
 ) => {
-  const writer = (data: string, section: string) => {
+  const writer = (data: string | Buffer, section: string) => {
     if (destination === 'stdout') {
       process.stdout.write(data);
     } else {
       const filename = path.join(destination, `aeas-${section}.${format}`);
-      fs.appendFileSync(filename, data);
+      if (format === 'csv') fs.appendFileSync(filename, data);
+      else fs.writeFileSync(filename, data);
     }
   };
 
   if (format === 'csv') {
-    formatImageDetails(output, writer);
-    if (output.partitionTable)
-      formatPartitionDetails(output.partitionTable, writer);
-    if (output.keywordFiles) formatKeywordFile(output.keywordFiles, writer);
-    if (output.renamedFiles) formatRenamedFile(output.renamedFiles, writer);
-    if (output.deletedFiles) formatDeletedFile(output.deletedFiles, writer);
+    csvImage(output, writer);
+    if (output.partitionTable) csvPartition(output.partitionTable, writer);
+    if (output.keywordFiles) csvKeywordFile(output.keywordFiles, writer);
+    if (output.renamedFiles) csvRenamedFile(output.renamedFiles, writer);
+    if (output.deletedFiles) csvDeletedFile(output.deletedFiles, writer);
   } else if (format === 'json') {
     writer(JSON.stringify(output), 'report');
+  } else if (format === 'pdf') {
+    pdfReport(output, writer);
   }
 };
 
-function formatImageDetails(
-  { imageHash, imageName }: ReportDetails,
-  writer: Writer
-) {
-  const section = 'image-details';
-  writer(`ImageName, ImageHash, FinalImageHash\n`, section);
-  writer(`'${imageName}', ${imageHash},''\n`, section);
+function pdfReport(output: ReportDetails, writer: Writer) {
+  const doc = new jsPDF('p', 'mm', 'a4'); //a4 is 210mm
+  doc.setFontSize(14);
+  let y = 5;
+  doc.setFontSize(20);
+  const section = 'AEAS Generated Report';
+  doc.text(section, 105 - doc.getTextWidth(section) / 2, y);
+  y += doc.getLineHeight();
+  doc.setFontSize(14);
+  y = pdfImage(output, doc, y);
+  if (output.partitionTable) y = pdfPartition(output.partitionTable, doc, y);
+  if (output.deletedFiles) y = pdfDeletedFile(output.deletedFiles, doc, y);
+  if (output.renamedFiles) y = pdfRenamedFile(output.renamedFiles, doc, y);
+  if (output.keywordFiles) y = pdfKeywordFile(output.keywordFiles, doc, y);
+  const arrayBuffer = doc.output('arraybuffer');
+  writer(Buffer.from(arrayBuffer), 'report');
 }
 
-function formatPartitionDetails(
+function pdfImage(
+  { imageHash, imageName }: ReportDetails,
+  doc: jsPDF,
+  y: number
+): number {
+  let height = y;
+  const section = 'Image Details';
+  doc.text(section, 105 - doc.getTextWidth(section) / 2, height);
+  height += doc.getLineHeight();
+
+  doc.setFontSize(9);
+  doc.text(doc.splitTextToSize(`Image Name: ${imageName}`, 200), 10, height);
+  height += doc.getLineHeight();
+  doc.text(
+    doc.splitTextToSize(`Image Hash (md5): ${imageHash?.md5sum}`, 200),
+    10,
+    height
+  );
+  height += doc.getLineHeight();
+  doc.text(
+    doc.splitTextToSize(`Image Hash (sha1): ${imageHash?.sha1sum}`, 200),
+    10,
+    height
+  );
+  height += doc.getLineHeight();
+  doc.text(doc.splitTextToSize(`Image Hash Final: `, 200), 10, height);
+  height += doc.getLineHeight();
+  doc.setFontSize(14);
+  return height;
+}
+
+function pdfPartition(
   partitionTable: PartitionTable,
-  writer: Writer
-) {
+  doc: jsPDF,
+  y: number
+): number {
+  let height = y;
+  const section = 'Partition Details';
+  const partitionBody: string[][] = [];
+  doc.text(section, 105 - doc.getTextWidth(section) / 2, height);
+  height += doc.getLineHeight();
+
+  doc.setFontSize(9);
+  doc.text(
+    doc.splitTextToSize(
+      `Table Type: ${partitionTable.tableType}, Sector Size: ${partitionTable.sectorSize}`,
+      200
+    ),
+    10,
+    height
+  );
+  height += doc.getLineHeight();
+  doc.setFontSize(14);
+  for (const partition of partitionTable.partitions) {
+    partitionBody.push([
+      partition.description,
+      `${partition.start}`,
+      `${partition.length}`,
+      `${partition.end}`,
+    ]);
+  }
+  doc.autoTable({
+    head: [['Description', 'Start', 'Length', 'End']],
+    body: partitionBody,
+    startY: height,
+  });
+  return doc.lastAutoTable.finalY + doc.getLineHeight();
+}
+
+function pdfDeletedFile(deletedFiles: File[], doc: jsPDF, y: number): number {
+  let height = y;
+  const section = 'Deleted Files';
+  const deletedBody: string[][] = [];
+  doc.text(section, 105 - doc.getTextWidth(section) / 2, height);
+  height += doc.getLineHeight();
+  for (const deleted of deletedFiles) {
+    deletedBody.push([
+      deleted.inode,
+      deleted.fileName,
+      `${deleted.size}`,
+      deleted.mtime,
+      deleted.atime,
+      deleted.ctime,
+      deleted.hash,
+    ]);
+  }
+  if (deletedBody.length > 0) {
+    doc.autoTable({
+      head: [
+        ['Inode', 'Filename', 'Size', 'Modified', 'Accessed', 'Create', 'Hash'],
+      ],
+      body: deletedBody,
+      startY: height,
+    });
+    return doc.lastAutoTable.finalY + doc.getLineHeight();
+  } else {
+    doc.text('No Deleted Files Found', 50, height);
+    return height + doc.getLineHeight();
+  }
+}
+
+function pdfRenamedFile(
+  renamedFiles: RenamedFile[],
+  doc: jsPDF,
+  y: number
+): number {
+  let height = y;
+  const section = 'Renamed Files';
+  const renamedBody: string[][] = [];
+  doc.text(section, 105 - doc.getTextWidth(section) / 2, height);
+  height += doc.getLineHeight();
+  for (const renamed of renamedFiles) {
+    renamedBody.push([
+      renamed.file.inode,
+      renamed.file.fileName,
+      renamed.trueExtensions.join(','),
+      `${renamed.file.size}`,
+      renamed.file.mtime,
+      renamed.file.atime,
+      renamed.file.ctime,
+      renamed.file.hash,
+    ]);
+  }
+
+  if (renamedBody.length > 0) {
+    doc.autoTable({
+      head: [
+        [
+          'Inode',
+          'Filename',
+          'True Ext.',
+          'Size',
+          'Modified',
+          'Accessed',
+          'Create',
+          'Hash',
+        ],
+      ],
+      body: renamedBody,
+      startY: height,
+    });
+    return doc.lastAutoTable.finalY + doc.getLineHeight();
+  } else {
+    doc.text('No Renamed Files Found', 50, height);
+    return height + doc.getLineHeight();
+  }
+}
+
+function pdfKeywordFile(
+  keywordFiles: KeywordFile[],
+  doc: jsPDF,
+  y: number
+): number {
+  let height = y;
+  const section = 'Keyword Files';
+  const keywordBody: string[][] = [];
+  doc.text(section, 105 - doc.getTextWidth(section) / 2, height);
+  height += doc.getLineHeight();
+  for (const keyword of keywordFiles) {
+    keywordBody.push([
+      keyword.file.inode,
+      keyword.file.fileName,
+      keyword.matches.join(','),
+      `${keyword.file.size}`,
+      keyword.file.mtime,
+      keyword.file.atime,
+      keyword.file.ctime,
+      keyword.file.hash,
+    ]);
+  }
+  if (keywordBody.length > 0) {
+    doc.autoTable({
+      head: [
+        [
+          'Inode',
+          'Filename',
+          'Matches',
+          'Size',
+          'Modified',
+          'Accessed',
+          'Create',
+          'Hash',
+        ],
+      ],
+      body: keywordBody,
+      startY: height,
+    });
+    return doc.lastAutoTable.finalY + doc.getLineHeight();
+  } else {
+    doc.text('No Keyword Files Found', 50, height);
+    return height + doc.getLineHeight();
+  }
+}
+
+function csvImage({ imageHash, imageName }: ReportDetails, writer: Writer) {
+  const section = 'image-details';
+  writer(
+    `ImageName, ImageHash (md5), ImageHash (sha1) FinalImageHash\n`,
+    section
+  );
+  writer(
+    `'${imageName}', ${imageHash.md5sum}, ${imageHash?.sha1sum},''\n`,
+    section
+  );
+}
+
+function csvPartition(partitionTable: PartitionTable, writer: Writer) {
   const section = 'partitions';
   writer(`TableType, SectorSize, Description, Start, Length, End\n`, section);
   for (const partition of partitionTable.partitions) {
@@ -62,7 +272,7 @@ function formatPartitionDetails(
   }
 }
 
-function formatRenamedFile(renamedFiles: RenamedFile[], writer: Writer) {
+function csvRenamedFile(renamedFiles: RenamedFile[], writer: Writer) {
   const section = 'renamed-files';
   writer(
     `Inode,Filename,True Extension, Size, Modified, Accessed, Created, Hash\n`,
@@ -77,7 +287,7 @@ function formatRenamedFile(renamedFiles: RenamedFile[], writer: Writer) {
   }
 }
 
-function formatDeletedFile(deletedFiles: File[], writer: Writer) {
+function csvDeletedFile(deletedFiles: File[], writer: Writer) {
   const section = 'deleted-files';
   writer(`Inode,Filename, Size, Modified, Accessed, Created, Hash\n`, section);
   for (const deleted of deletedFiles) {
@@ -88,7 +298,7 @@ function formatDeletedFile(deletedFiles: File[], writer: Writer) {
   }
 }
 
-function formatKeywordFile(keywordFiles: KeywordFile[], writer: Writer) {
+function csvKeywordFile(keywordFiles: KeywordFile[], writer: Writer) {
   const section = 'keyword-match-files';
   writer(
     `Inode,Filename,Matches, Size, Modified, Accessed, Created, Hash\n`,
--- src/renderer/Report.tsx
+++ src/renderer/Report.tsx
@@ -6,15 +6,12 @@ import KeywordFilesComponent from './KeywordFiles';
 import DeletedFilesComponent from './DeletedFiles';
 import './Report.css';
 import { useNavigate } from 'react-router-dom';
-import jsPDF from 'jspdf';
-import { text } from 'stream/consumers';
 
 export default function ReportComponent() {
   const [reportReady, setReportReady] = useState(false);
   const [message, setMessage] = useState('Finding Image...');
   const [details, setDetails] = useState<ReportDetails>();
   const navigate = useNavigate();
-  const reportRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
     window.electron.ipcRenderer.on(
@@ -37,26 +34,7 @@ export default function ReportComponent() {
     window.electron.ipcRenderer.on('select-dir', ([dir]) => {
       if (dir === undefined) return;
 
-      if (format === 'pdf' && reportRef.current !== null) {
-        const doc = new jsPDF('l', 'pt', 'a4');
-        doc.html(reportRef.current, {
-          callback: function (doc) {
-            window.electron.ipcRenderer.sendMessage('print', [
-              format,
-              dir,
-              doc.output('arraybuffer'),
-            ]);
-          },
-          autoPaging: 'text',
-          width: 100,
-        });
-      }
-
-      window.electron.ipcRenderer.sendMessage('print', [
-        format,
-        dir,
-        reportRef.current,
-      ]);
+      window.electron.ipcRenderer.sendMessage('print', [format, dir]);
     });
     window.electron.ipcRenderer.sendMessage('select-dir', []);
   }
@@ -69,12 +47,13 @@ export default function ReportComponent() {
         {reportReady ? <PrintButton onPrint={handlePrint} /> : null}
       </header>
       {reportReady ? (
-        <article ref={reportRef} className="report">
+        <article className="report">
           <p>
             <strong>Image:</strong> {details?.imageName}
           </p>
           <h3>Image Hashes</h3>
-          <p>{JSON.stringify(details?.imageHash)}</p>
+          <p>md5: {details?.imageHash?.md5sum}</p>
+          <p>sah1: {details?.imageHash?.sha1sum}</p>
           {details?.partitionTable ? (
             <PartitionTableComponent partitionTable={details.partitionTable} />
           ) : null}
