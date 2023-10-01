548031fd8c47798d81fba52c5e6294f712dbf91f
JoeySuttonPreece <102568393@student.swin.edu.au>
2023-10-02 02:28:33 +1100

    ref: tidy events.ts

--- src/main/events.ts
+++ src/main/events.ts
@@ -1,27 +1,19 @@
 import { ipcMain, dialog } from 'electron';
 
-import { listFiles } from '../domain/file-system-tools';
-import {
-  ReportDetails,
-  orchestrator,
-  validateImage,
-} from '../domain/orchestrator';
-import { getHashAsync, getSearchStringAsync } from '../domain/other-cli-tools';
-import { getPartitionTable } from '../domain/volume-system-tools';
+import { orchestrator, validateImage } from '../domain/orchestrator';
 import { Print } from './output-parser';
 
 ipcMain.on('do-everything', async (event, [options]) => {
   // insert loading while orchestrator is going, this means we can't await,
   // perhaps a callback is put into orchestrator to define what it should do??
-  let output = {} as ReportDetails | null;
 
   try {
-    output = await orchestrator(options, (msg) => {
+    const output = await orchestrator(options, (msg) => {
       event.sender.send('status:update', msg);
     });
 
     ipcMain.on('select-dir', (event) => {
-      let path = dialog.showOpenDialogSync({
+      const path = dialog.showOpenDialogSync({
         properties: ['openDirectory'],
       });
       console.log(path);
