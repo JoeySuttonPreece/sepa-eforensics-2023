f4d63988a34804cf865e95387bfbd9edade0c0b1
tnguyenSwin <101607169@student.swin.edu.au>
2023-10-13 14:37:20 +1100

    Update logic on mounting

--- src/main/main.ts
+++ src/main/main.ts
@@ -159,6 +159,9 @@ app
       }).argv;
 
     if (commandLine) {
+      imagePath = await validateZip(imagePath);
+      imagePath = await validateDMG(imagePath);
+      
       if ((await validateImage(imagePath)) === false || imagePath == null) {
         console.log(
           `Image could not be found or is incorrect type: imagePath was ${imagePath}}`
@@ -166,8 +169,6 @@ app
         return;
       }
 
-      imagePath = await validateDMG(imagePath);
-      imagePath = await validateZip(imagePath);
       if (include == null) {
         include = ['p', 'd', 'r', 'c', 'k', 't']; // 's' (for save carved files)
       } else {
