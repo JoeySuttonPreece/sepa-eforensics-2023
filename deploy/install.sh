1d38cfa323e2d5b5b39203fac0140998647e6353
Rob Findlay <103613414@student.swin.edu.au>
2023-10-14 11:24:49 +1100

    Just make sure that everything is executable first

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,6 +4,8 @@ echo "BECOME password is your sudo password"
 ansible-playbook playbook.yaml --ask-become-pass
 sudo mkdir -p /opt/AEAS/
 sudo cp ElectronReact-4.6.0.AppImage /opt/AEAS/AEAS.AppImage
+sudo chmod +x /opt/AEAS/AEAS.AppImage
 sudo cp ICON-V1.gif /opt/AEAS/ICON-V1.gif
 sudo cp AEAS /usr/local/bin/AEAS
+sudo chmod +x /usr/local/bin/AEAS
 sudo cp AEAS.desktop /usr/local/share/applications/AEAS.desktop
